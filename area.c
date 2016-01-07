d4b96877f6410b0ed14cb8c0e9c7fe7c4da84fda
Zaid Hoona <zdhoona12@gmail.com>
2016-01-07 11:10:32 +0530

    Adding spaces
    Adding spaces properly making the code easy to read

--- area.c
+++ area.c
@@ -7,29 +7,29 @@ void main() {
 	clrscr();
 	printf("positive values only!\n");
 	printf("Find the area of : \n(rect:r,circ:c,triangle:t): ");
-	scanf("%c",&shape);
+	scanf("%c", &shape);
 
-	if(shape == 'r'){
+	if(shape == 'r') {
 		printf("enter length: ");
-		scanf("%d",&dat1);
+		scanf("%d", &dat1);
 		printf("enter breadth: ");
-		scanf("%d",&dat2);
-		area=dat1*dat2;
-		printf("area of rectangle: %d",area);
-	}else if(shape == 'c'){
+		scanf("%d", &dat2);
+		area = dat1 * dat2;
+		printf("area of rectangle: %d", area);
+	} else if(shape == 'c') {
 		printf("enter radius: ");
-		scanf("%d",&dat1);
-		area= 3.142*dat1*dat1;
-		printf("area of circle : %d",area);
-	}else if(shape == 't'){
+		scanf("%d", &dat1);
+		area= 3.142 * dat1 * dat1;
+		printf("area of circle : %d", area);
+	} else if(shape == 't') {
 		printf("enter height :");
-		scanf("%d",&dat1);
+		scanf("%d", &dat1);
 		printf("enter base: ");
-		scanf("%d",&dat2);
+		scanf("%d", &dat2);
 		area = .5 * dat1 * dat2;
-		printf("area of triangle: %d",area);
-	}else{
-	printf("shape not recognised");
+		printf("area of triangle: %d", area);
+	} else {
+		printf("shape not recognised");
 	}
 	getch();
 }
