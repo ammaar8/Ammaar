f1fc897f6c0acbcd2384676a77eea701d1fb39bb
ammaar8 <asketch8@gmail.com>
2016-01-10 22:46:28 -0800

    Some changes
    Added spacing and defined PI for performing operations

--- areavol.c
+++ areavol.c
@@ -17,34 +17,32 @@ void main(){
 }
 // Code for Volume :
 void volume(){
+
         float dat1,dat2,dat3,vol;
         char solid;
         printf("Square:s \nRectangle:r \nSphere:c \n");
-        scanf(" %c",&solid);
+        scanf(" %c", &solid);
+        #define PI 3.142
         if (solid == 's'){
             printf("input side:");
-            scanf("%f",&dat1);
+            scanf("%f", &dat1);
             vol = dat1*dat1*dat1;
             printf("Volume of required shape is %.2f ", vol);
         }else if(solid == 'r'){
             printf("input length:");
-            scanf(" %f",&dat1);
+            scanf(" %f", &dat1);
             printf("input breadth:");
-            scanf(" %f",&dat2);
+            scanf(" %f", &dat2);
             printf("input height:");
-            scanf(" %f",&dat3);
+            scanf(" %f", &dat3);
             vol = dat1*dat2*dat3;
             printf("Volume of required shape is %.2f ", vol);
-        }/*else if(solid == 'c'){
-            float pi,constant;
-            pi = 22/7;
-            constant = 4/3;
-
+        }else if(solid == 'c'){
             printf("input radius:");
             scanf(" %f",&dat1);
-            vol = constant * pi * dat1 * dat1 * dat1;
+            vol = 1.3333 * PI * dat1 * dat1 * dat1;
             printf("Volume of required solid is %.2f ", vol);
-        }*/else{
+        }else{
             printf("solid not recognized");
         }
 }
@@ -55,17 +53,17 @@ void area(){
         float dat1,dat2,dat3,area;
         char lamina;
         printf("Square:s \nRectangle:r \nCircle:c \n");
-        scanf(" %c",&lamina);
+        scanf(" %c", &lamina);
         if (lamina == 's'){
             printf("input side:");
-            scanf("%f",&dat1);
+            scanf("%f", &dat1);
             area = dat1*dat1;
             printf("Area of required solid is %.2f ", area);
         }else if (lamina == 'r'){
             printf("input length:");
-            scanf("%f",&dat1);
+            scanf("%f", &dat1);
             printf("input breadth:");
-            scanf("%f",&dat2);
+            scanf("%f", &dat2);
             area = dat1*dat2;
             printf("Area of required solid is %.2f ", area);
         }
