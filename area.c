6cbb6705bcfac5ea00f287edfd79cf30790d9d4f
ammaar8 <asketch8@gmail.com>
2016-01-10 18:46:54 -0800

    Change of variable type
    type of variable changed to float to allow display of decimal values for
    more precise answers

--- area.c
+++ area.c
@@ -2,32 +2,32 @@
 #include<conio.h>
 
 void main() {
-	int dat1,dat2,area;
+	float dat1,dat2,area;
 	char shape;
-	clrscr();
+
 	printf("positive values only!\n");
 	printf("Find the area of : \n(rect:r,circ:c,triangle:t): ");
 	scanf("%c",&shape);
 
 	if(shape == 'r'){
 		printf("enter length: ");
-		scanf("%d",&dat1);
+		scanf("%f",&dat1);
 		printf("enter breadth: ");
-		scanf("%d",&dat2);
+		scanf("%f",&dat2);
 		area=dat1*dat2;
-		printf("area of rectangle: %d",area);
+		printf("area of rectangle: %.2f",area);
 	}else if(shape == 'c'){
 		printf("enter radius: ");
-		scanf("%d",&dat1);
+		scanf("%f",&dat1);
 		area= 3.142*dat1*dat1;
-		printf("area of circle : %d",area);
+		printf("area of circle : %.2f",area);
 	}else if(shape == 't'){
 		printf("enter height :");
-		scanf("%d",&dat1);
+		scanf("%f",&dat1);
 		printf("enter base: ");
-		scanf("%d",&dat2);
+		scanf("%f",&dat2);
 		area = .5 * dat1 * dat2;
-		printf("area of triangle: %d",area);
+		printf("area of triangle: %.2f",area);
 	}else{
 	printf("shape not recognised");
 	}
