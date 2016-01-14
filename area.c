fda7df96953834de3710f301a6cc3cc6105c267f
Zaid Hoona <zdhoona12@gmail.com>
2016-01-14 18:23:35 +0530

    Resolving conflicts
    Resolving merge conflicts for the commit
    :(20af7ee14c7904a03b1049f242e5b4c0d01a1185)

--- area.c
+++ area.c
@@ -10,11 +10,6 @@
 #define HALF 0.5
 
 void main() {
-<<<<<<< HEAD
-	float dat1,dat2,area;
-	char shape;
-
-=======
 	int dat1,dat2;
 	/*
 		Area should be double because the area of the circle 
@@ -24,34 +19,12 @@ void main() {
 	char shape;
 	
 	clrscr();
->>>>>>> origin/master
 	printf("positive values only!\n");
 	printf("Find the area of : \n(rect:r,circ:c,triangle:t): ");
 	scanf("%c", &shape);
 
 	if(shape == 'r') {
 		printf("enter length: ");
-<<<<<<< HEAD
-		scanf("%f",&dat1);
-		printf("enter breadth: ");
-		scanf("%f",&dat2);
-		area=dat1*dat2;
-		printf("area of rectangle: %.2f",area);
-	}else if(shape == 'c'){
-		printf("enter radius: ");
-		scanf("%f",&dat1);
-		area= 3.142*dat1*dat1;
-		printf("area of circle : %.2f",area);
-	}else if(shape == 't'){
-		printf("enter height :");
-		scanf("%f",&dat1);
-		printf("enter base: ");
-		scanf("%f",&dat2);
-		area = .5 * dat1 * dat2;
-		printf("area of triangle: %.2f",area);
-	}else{
-	printf("shape not recognised");
-=======
 		scanf("%d", &dat1);
 		printf("enter breadth: ");
 		scanf("%d", &dat2);
@@ -72,7 +45,6 @@ void main() {
 		printf("area of triangle: %.2f", area);
 	} else {
 		printf("shape not recognised");
->>>>>>> origin/master
 	}
 	getch();
 }
